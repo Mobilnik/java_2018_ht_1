a89bd1eded4f3a258cb3701ae6a5f9b6ae3f93a7
VladislavBel <vladbel1999@gmail.com>
2018-09-25 22:54:28 +0300

    Changed messege

--- src/ru/milandr/courses/Application.java
+++ src/ru/milandr/courses/Application.java
@@ -2,6 +2,6 @@ package ru.milandr.courses;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello nikita ne igaray!");
     }
 }
