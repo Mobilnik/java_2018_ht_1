b19a44909cac563ac3e62b7f82d7797570d8b134
Arseny <mr.marshinin@mail.ru>
2018-09-30 13:30:02 +0300

    task is done

--- src/ru/milandr/courses/point.java
+++ src/ru/milandr/courses/point.java
@@ -5,12 +5,22 @@ public class point {
     private double x;
     private double y;
 
-    public point(double x, double y){
+    private point(double x, double y){
         this.x = x;
         this.y = y;
     }
 
-    public double getDistance(point a){
+    private double getDistance(point a){
         return Math.sqrt((this.x - a.x) * (this.x - a.x) + (this.y - a.y) * (this.y - a.y));
+    }
+
+    private point getMiddlePoint(point a){
+        return new point((this.x + a.x)/2,(this.y + a.y)/2);
+    }
+
+    public String getCircle(point a) {
+        point middle = this.getMiddlePoint(a);
+        double RadiusSquare = (this.getDistance(a)/2)*(this.getDistance(a)/2);
+        return String.format("Ecuation is (x - (%.2f))^2 + (y - (%.2f)^2 = %.2f)", middle.x, middle.y, RadiusSquare);
     }
 }
