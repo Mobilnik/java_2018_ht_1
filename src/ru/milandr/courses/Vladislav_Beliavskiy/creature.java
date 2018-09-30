0a10ca689f6493e4987c35574c6600d34f092dac
VladislavBel <vladbel1999@gmail.com>
2018-09-30 17:39:28 +0300

    Changed messege

---
+++ src/ru/milandr/courses/Vladislav_Beliavskiy/creature.java
@@ -0,0 +1,60 @@
+package ru.milandr.courses.Vladislav_Beliavskiy;
+
+public class creature {
+    private int NumberOfLegs;
+    private int NumberOfEyes;
+    private int NumberOfEars;
+    private int Age;
+    private String name;
+
+    public int getNumberOfLegs ()
+    {
+        return this.NumberOfLegs;
+    }
+
+    public void setNumberOfLegs (int NumberOfLegs)
+    {
+        this.NumberOfLegs = NumberOfLegs;
+    }
+
+    public int getNumberOfEyes ()
+    {
+        return this.NumberOfEyes;
+    }
+
+    public void setNumberOfEyes (int NumberOfEyes)
+    {
+        this.NumberOfEyes = NumberOfEyes;
+    }
+
+    public int getNumberOfEars ()
+    {
+        return this.NumberOfEars;
+    }
+
+    public void setNumberOfEars (int NumberOfEars)
+    {
+        this.NumberOfEars = NumberOfEars;
+    }
+
+    public int getAge ()
+    {
+        return this.Age;
+    }
+
+    public void setAge (int Age)
+    {
+        this.Age = Age;
+    }
+
+    public String getName ()
+    {
+        return this.name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+}
